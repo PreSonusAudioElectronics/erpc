b0e7ed2729fdd34488609e52b0994052957bd992
danderson-presonus <danderson@presonus.com>
2021-06-18 10:15:58 -0700

    add include guards

--- erpc_c/common/linux/fsl_redirect.h
+++ erpc_c/common/linux/fsl_redirect.h
@@ -1,7 +1,11 @@
 /*
-*   Throwaway redirection of NXP erpc example to zephyr
+*   Throwaway redirection of NXP erpc example to linux
 */
+#ifndef _fsl_redirect_h
+#define _fsl_redirect_h
 
 #include <cstdio>
 
 #define PRINTF (printf)
+
+#endif // _fsl_redirect_h
--- erpc_c/common/zephyr/fsl_redirect.h
+++ erpc_c/common/zephyr/fsl_redirect.h
@@ -1,7 +1,11 @@
 /*
 *   Throwaway redirection of NXP erpc example to zephyr
 */
+#ifndef _fsl_redirect_h
+#define _fsl_redirect_h
 
 #include <kernel.h>
 
 #define PRINTF (printk)
+
+#endif // _fsl_redirect_h
